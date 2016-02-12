798172230c6d50691e693a541a212a78274d9da2
jessetmoy <jesse.tmoy@gmail.com>
2016-02-12 09:58:27 -0500

    gh-pages test update 2

--- js/assignment_3.js
+++ js/assignment_3.js
@@ -1,6 +1,7 @@
 // Jesse Moy Interactive Web Mapping Assignment 3
 
-// gh- page update test
+// gh-pages update test
+// gh-pages update test 2
 
 // varible for our map container setting centroid [lat, lng] and zoom
 var map = L.map('map').setView([40.71,-73.93], 11);
